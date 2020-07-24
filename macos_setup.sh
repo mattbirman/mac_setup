7bef93b4fca9e9736b41e6f06438a674839c7320
Matt Birman <mbirman@zendesk.com>
2020-07-24 11:39:27 +1000

    Remove unused brew and move zendesk to bottom

--- macos_setup.sh
+++ macos_setup.sh
@@ -20,12 +20,8 @@ fi
 brew update-reset
 brew tap
 brew update
-brew install coreutils git python vim fish tmux go goenv rbenv ag bat
+brew install coreutils git python vim fish tmux ag bat
 brew cask install alfred dozer 1password fluor
-
-brew tap AdoptOpenJDK/openjdk
-brew cask install adoptopenjdk8
-brew install sbt
 
 stack install apply-refact hlint stylish-haskell hasktags hoogle
 
@@ -39,11 +35,10 @@ cd ..
 rm -fr fonts
 
 # setup fish
+rm -rf ~/.local/share/omf
 curl -s -L https://get.oh-my.fish > omf-install.fish
 fish omf-install.fish --noninteractive
 rm omf-install.fish
-fish -c 'omf install theme agnoster'
-fish -c 'omf theme agnoster'
 fish -c 'omf install bass'
 fish -c 'omf install bobthefish'
 
@@ -69,3 +64,9 @@ popd
 // install vim config
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+// Zendesk
+brew tap AdoptOpenJDK/openjdk
+brew cask install adoptopenjdk8
+brew install sbt
+
